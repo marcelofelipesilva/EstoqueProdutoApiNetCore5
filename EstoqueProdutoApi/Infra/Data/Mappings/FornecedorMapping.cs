47fb8efedcef6de21b81d53e69ddec2c870f395d
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-27 19:08:37 -0300

    add FornecedorMapping.cs

---
+++ EstoqueProdutoApi/Infra/Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,18 @@
+using EstoqueProdutoApi.Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EstoqueProdutoApi.Infra.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.ToTable("TB_Fornecedor");
+            builder.HasKey(p => p.FuncionarioID);
+            builder.Property(p => p.FuncionarioID).ValueGeneratedOnAdd();
+            builder.Property(p => p.Nome);
+            
+        }
+    }
+}
