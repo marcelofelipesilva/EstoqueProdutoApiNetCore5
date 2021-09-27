b873ba40feea280079840c7a0724565c4f47f9da
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-27 19:19:11 -0300

    add ContaAcessoRepository.cs

---
+++ EstoqueProdutoApi/Infra/Data/Repositories/ContaAcessoRepository.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using EstoqueProdutoApi.Business.Entities;
+using EstoqueProdutoApi.Business.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstoqueProdutoApi.Infra.Data.Mappings
+{
+    public class ContaAcessoRepository : IContaAcessoRepository
+    {
+        private readonly EstoqueProdutoApiDbcontext _apiDbcontext;
+
+        public ContaAcessoRepository(EstoqueProdutoApiDbcontext apiDbcontext)
+        {
+            _apiDbcontext = apiDbcontext;
+        }
+
+        public void AdicionarConatAcesso(ContaAcesso contaAcesso)
+        {
+            _apiDbcontext.Add(contaAcesso) ;
+        }
+
+        public void Commit()
+        {
+            _apiDbcontext.SaveChanges() ;
+        }
+
+        public List<ContaAcesso> obterContaAcessos(int ContaAcessoID)
+        {
+            return _apiDbcontext.Livros.Include(i => i.ContaAcessoID).Where(w => w.ContaAcessoID == ContaAcessoID).ToList();
+        }
+    }
+}
