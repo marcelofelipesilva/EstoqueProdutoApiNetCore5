e384aacaf4f7639aeb2131352f88f560286ee19e
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-28 12:26:54 -0300

    add FornecedorController.cs

---
+++ EstoqueProdutoApi/Controllers/FornecedorController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EstoqueProdutoApi.Business.Entities;
+using EstoqueProdutoApi.Infra.Data;
+
+namespace EstoqueProdutoApi.Controllers
+{
+    [Route("api/v1/fornecedor")]
+    [ApiController]
+    public class FornecedorController : ControllerBase
+    {
+        private readonly EstoqueProdutoApiDbcontext _context;
+
+        public FornecedorController(EstoqueProdutoApiDbcontext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Fornecedor
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Fornecedor>>> GetFornecedores()
+        {
+            return await _context.Fornecedores.ToListAsync();
+        }
+
+        // GET: api/Fornecedor/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Fornecedor>> GetFornecedor(int id)
+        {
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return fornecedor;
+        }
+
+        // PUT: api/Fornecedor/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFornecedor(int id, Fornecedor fornecedor)
+        {
+            if (id != fornecedor.FornecedorID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fornecedor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FornecedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Fornecedor
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Fornecedor>> PostFornecedor(Fornecedor fornecedor)
+        {
+            _context.Fornecedores.Add(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFornecedor", new { id = fornecedor.FornecedorID }, fornecedor);
+        }
+
+        // DELETE: api/Fornecedor/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFornecedor(int id)
+        {
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fornecedores.Remove(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FornecedorExists(int id)
+        {
+            return _context.Fornecedores.Any(e => e.FornecedorID == id);
+        }
+    }
+}
