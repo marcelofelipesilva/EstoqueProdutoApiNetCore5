c3278454037d77f9c3c623edc07ea05afe071913
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-28 11:44:52 -0300

    add ContaAcessoViewModelOutput.cs

---
+++ EstoqueProdutoApi/Models/ContaAcesso/ContaAcessoViewModelOutput.cs
@@ -0,0 +1,11 @@
+namespace EstoqueProdutoApi.Models.ContaAcesso
+{
+    public class ContaAcessoViewModelOutput
+    {
+        public string Nome { get; set; }
+        public int Matricula { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
