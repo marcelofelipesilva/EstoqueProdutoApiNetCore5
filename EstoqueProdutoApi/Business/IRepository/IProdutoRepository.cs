3aeb85991f2e368d5ebd1e14374598c6aaf759df
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-28 11:08:21 -0300

    corrigindo erro digitação

--- EstoqueProdutoApi/Business/IRepository/IProdutoRepository.cs
+++ EstoqueProdutoApi/Business/IRepository/IProdutoRepository.cs
@@ -5,7 +5,7 @@ namespace EstoqueProdutoApi.Business.IRepository
 {
     public interface IProdutoRepository
     {
-        void AdicionarConatAcesso(Produto produto);
+        void AdicionarProduto(Produto produto);
         void Commit();
 
         List<Produto> obterProdutos(int ProdutoID);
